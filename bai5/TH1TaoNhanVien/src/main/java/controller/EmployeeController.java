9ff5745cd1187c09d0d413d49ff91960c597ac12
vu viet <vvviet97@gmail.com>
2022-07-16 16:26:14 +0700

    bai5TH1

---
+++ bai5/TH1TaoNhanVien/src/main/java/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+@RequestMapping("employee")
+public class EmployeeController {
+
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employee/info";
+    }
+}
