ecefdb2dbda7a8de9f5e01bdf7fda58cf64610f1
vu viet <vvviet97@gmail.com>
2022-07-16 17:43:20 +0700

    bai5TH3

---
+++ bai5/TH3UploadFile/src/main/java/service/ProductService.java
@@ -0,0 +1,47 @@
+package service;
+
+import model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductService implements IProductService {
+    private List<Product> products = new ArrayList<>();
+
+    @Override
+    public List<Product> findAll() {
+        return products;
+    }
+
+    @Override
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        for (Product p : products) {
+            if (p.getId() == id) {
+                p = product;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == id) {
+                products.remove(i);
+                break;
+            }
+        }
+    }
+}
